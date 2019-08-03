1d2de99772a0a430c3c2a5f2d1b88b1c5ce22e43
Hiring <Hiring@WKWIN6691238.global.publicisgroupe.net>
2019-08-03 16:55:50 +0530

    converted JAR to WAR

--- src/main/java/com/starwars/StartStarWarsApplication.java
+++ src/main/java/com/starwars/StartStarWarsApplication.java
@@ -2,12 +2,19 @@ package com.starwars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StartStarWarsApplication {
+public class StartStarWarsApplication extends SpringBootServletInitializer{
 
     // start everything
     public static void main(String[] args) {
         SpringApplication.run(StartStarWarsApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StartStarWarsApplication.class);
     }
 }
